494e6881f2805d38c228e8b5cc6c04005bafbb3b
JhonneyQ <kananqadirov2005@gmail.com>
2025-06-23 17:02:30 +0400

    gommit kanan

--- StartBootstrap/src/App.jsx
+++ StartBootstrap/src/App.jsx
@@ -8,7 +8,18 @@ function App() {
 
   return (
     <>
-      
+      <section className='phone'> <div className='container'>
+        <div className='all'>
+          <div className='par'>
+            <h2>Showcase your app beautifully.</h2> <p>Launch your mobile app landing page faster with this free, open source theme from Start Bootstrap!</p>
+            <div className='plays'>
+              <img src="https://startbootstrap.github.io/startbootstrap-new-age/assets/img/google-play-badge.svg" alt="" /> <img src='https://startbootstrap.github.io/startbootstrap-new-age/assets/img/app-store-badge.svg' />
+            </div>
+          </div>
+          <img src='https://www.freeiconspng.com/uploads/apple-iphone-x-pictures-5.png' />
+        </div>
+      </div>
+      </section>
     </>
   )
 }
